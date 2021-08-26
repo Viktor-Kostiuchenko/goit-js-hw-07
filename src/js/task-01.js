55d240143c6eac7ee446428ef553e4374a0e2390
Viktor Kostiuchenko <kostiuchenkoviktor@gmail.com>
2021-08-26 23:48:04 +0300

    Complete the first task

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,2 +1,30 @@
+//* вариант А 
+// const categoriesEl = document.querySelector('#categories')
+// console.log(`В списке ${categoriesEl.children.length} категории.`)
 
 
+
+// for (const element of categoriesEl.children) {
+//   console.log(`Категория: ${element.firstElementChild.textContent}`)
+
+//   console.log(`Количество элементов: ${element.lastElementChild.children.length}`)
+  
+// }
+
+
+
+//* вариант B (больше как вопрос - можно ли вот так, через функцию решить данную таску?)
+
+// const getCategoriesInfo = categories => {
+
+//   console.log(`В списке ${categories.children.length} категории.`)
+
+//   for (const element of categories.children) {
+//     console.log(`Категория: ${element.firstElementChild.textContent}`)
+
+//     console.log(`Количество элементов: ${element.lastElementChild.children.length}`)
+//   }
+
+// }
+
+// getCategoriesInfo(document.querySelector('#categories'))
