d147053951d7152cf4a59945ac3bec93e41af484
Viktor Kostiuchenko <kostiuchenkoviktor@gmail.com>
2021-08-26 23:48:21 +0300

    Complete the second task

---
+++ src/js/task-02.js
@@ -0,0 +1,20 @@
+// const ingredients = [
+//   'Картошка',
+//   'Грибы',
+//   'Чеснок',
+//   'Помидоры',
+//   'Зелень',
+//   'Приправы',
+// ];
+
+// const listEl = document.querySelector('#ingredients')
+
+// const elements = ingredients.map(name => {
+//   const itemEl = document.createElement('li')
+//   itemEl.textContent = name
+
+//   return itemEl
+
+// })
+
+// listEl.append(...elements)
