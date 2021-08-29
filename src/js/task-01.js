c2b9935c4af9caa3fa339e60dbc04e5a3e6017c5
Viktor Kostiuchenko <kostiuchenkoviktor@gmail.com>
2021-08-30 01:38:01 +0300

    Fix the first task

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,32 +1,7 @@
-//* вариант А 
 const categoriesEl = document.querySelector('#categories')
 console.log(`В списке ${categoriesEl.children.length} категории.`)
 
-
-
 for (const element of categoriesEl.children) {
   console.log(`Категория: ${element.firstElementChild.textContent}`)
-
-  console.log(`Количество элементов: ${element.lastElementChild.children.length}`)
-  
+  console.log(`Количество элементов: ${element.lastElementChild.children.length}`) 
 }
-
-
-
-//* вариант B (больше как вопрос - можно ли вот так, через функцию решить данную таску?)
-
-// const categoriesEl = document.querySelector('#categories')
-
-// const getCategoriesInfo = categories => {
-
-//   console.log(`В списке ${categories.children.length} категории.`)
-
-//   for (const element of categories.children) {
-//     console.log(`Категория: ${element.firstElementChild.textContent}`)
-
-//     console.log(`Количество элементов: ${element.lastElementChild.children.length}`)
-//   }
-
-// }
-
-// getCategoriesInfo(categoriesEl)
