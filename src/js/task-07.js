b085c2b1915aa2d6bc2952f2a55831cccda204a6
Viktor Kostiuchenko <kostiuchenkoviktor@gmail.com>
2021-08-30 01:39:49 +0300

    Complete the seventh task

---
+++ src/js/task-07.js
@@ -0,0 +1,11 @@
+const refs = {
+  input: document.querySelector('#font-size-control'),
+  text: document.querySelector('#text')
+}
+
+refs.input.addEventListener('input', onChangeFontSize)
+
+function onChangeFontSize(event) {
+  refs.text.style.fontSize = `${event.currentTarget.value}px`
+}
+
