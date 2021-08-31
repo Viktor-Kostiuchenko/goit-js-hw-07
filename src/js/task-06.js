5937224c904f3cb9378bc888e4a3f76b780fe6d6
Viktor Kostiuchenko <kostiuchenkoviktor@gmail.com>
2021-09-01 01:52:23 +0300

    Refactor code for the sixth task

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -5,18 +5,15 @@ const refs = {
 refs.input.addEventListener('blur', onCheckDataLength)
 
 function onCheckDataLength(event) {
-    if (Number(event.currentTarget.dataset.length) === event.currentTarget.value.length) {
-      refs.input.classList.remove('invalid')
-      refs.input.classList.add('valid')
-    } else if (event.currentTarget.value.length === 0){
-      refs.input.classList.remove('invalid')
-      refs.input.classList.remove('valid')
-    } else {
-      refs.input.classList.remove('valid')
-      refs.input.classList.add('invalid')
-    }
+    Number(event.currentTarget.dataset.length) === event.currentTarget.value.length ?
+        setClass('valid', 'invalid') :
+        setClass('invalid', 'valid')
 }
 
+function setClass(add, remove) {
+  refs.input.classList.add(add)
+  refs.input.classList.remove(remove)
+}
 
 
 
